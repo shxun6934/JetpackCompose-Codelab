aafe96c43d6d63cfacf3b75106326b6094a43cc0
shxn6934 <b1612851@planet.kanazawa-it.ac.jp>
2022-11-26 15:52:35 +0900

    set TODO

--- app/src/main/java/com/shunichi/basic_codelab/ui/theme/Color.kt
+++ app/src/main/java/com/shunichi/basic_codelab/ui/theme/Color.kt
@@ -1,8 +1,3 @@
 package com.shunichi.basic_codelab.ui.theme
 
-import androidx.compose.ui.graphics.Color
-
-val Navy = Color(0xFF073042)
-val Blue = Color(0xFF4285F4)
-val LightBlue = Color(0xFFD7EFFE)
-val Chartreuse = Color(0xFFEFF7CF)
+// TODO: Define Color
